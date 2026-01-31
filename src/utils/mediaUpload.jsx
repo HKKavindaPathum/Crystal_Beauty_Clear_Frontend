9c947a5e9df5ebd521fffa162340541a8e1bca7f
HKKavindaPathum <kavindapathum98@gmail.com>
2026-01-31 15:39:16 +0530

    chnage superbase details

--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const url = "https://eiofvghmdzspvewawxhf.supabase.co"
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpb2Z2Z2htZHpzcHZld2F3eGhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNjg4OTgsImV4cCI6MjA2NDg0NDg5OH0.Vj3lZSmCbcLMpSNLpf3oVFEJnV2OZsfISjMzhMg3p_U"
+const url = "https://urgusrlvljuhfyupyhko.supabase.co"
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyZ3Vzcmx2bGp1aGZ5dXB5aGtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4NDk4MDEsImV4cCI6MjA4NTQyNTgwMX0.tau-ErjHVGcUkhC0yMG_87s3L6LMIGz_5MldOsalVq8"
 
 const supabase = createClient(url,key)
 
